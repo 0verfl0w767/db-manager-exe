b938040efcee9911a1300c80dfdae7c5900c8c1a
0verfl0w767 <tkddbs7631@naver.com>
2023-05-05 03:11:25 +0900

    Add dbAdd, dbDel, dbFind, Main..

--- main.py
+++ main.py
@@ -15,13 +15,17 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
+# from PySide6.QtUiTools import loadUiType
 
 from ui.main_ui import Ui_MainWindow as Main
+from ui.dbAdd_ui import Ui_MainWindow as Add
+from ui.dbDel_ui import Ui_MainWindow as Del
 from ui.dbFind_ui import Ui_MainWindow as Find
 
-# from PySide6.QtUiTools import loadUiType
-# form_class = loadUiType("./ui/main.ui")[0]
-# class Ui_MainWindow_Override(form_class):
+# Main = loadUiType("./ui/main.ui")[0]
+# Add = loadUiType("./ui/dbAdd.ui")[0]
+# Del = loadUiType("./ui/dbDel.ui")[0]
+# Find = loadUiType("./ui/dbFind.ui")[0]
 
 class Ui_MainWindow_Override(Main):
   def __init__(self):
@@ -47,6 +51,26 @@ class Ui_MainWindow_Override(Main):
     self.window = dbFindWindow()
     self.window.show()
 
+class Ui_dbAddWindow_Override(Add):
+  def __init__(self):
+    pass
+  
+  def setupUi(self, MainWindow):
+    super().setupUi(MainWindow)
+  
+  def retranslateUi(self, MainWindow):
+    super().retranslateUi(MainWindow)
+
+class Ui_dbDelWindow_Override(Del):
+  def __init__(self):
+    pass
+  
+  def setupUi(self, MainWindow):
+    super().setupUi(MainWindow)
+  
+  def retranslateUi(self, MainWindow):
+    super().retranslateUi(MainWindow)
+
 class Ui_dbFindWindow_Override(Find):
   def __init__(self):
     pass
@@ -57,10 +81,22 @@ class Ui_dbFindWindow_Override(Find):
   def retranslateUi(self, MainWindow):
     super().retranslateUi(MainWindow)
 
-class MainWindow(QMainWindow):
+class dbMainWindow(QMainWindow):
   def __init__(self):
-    super(MainWindow, self).__init__()
+    super(dbMainWindow, self).__init__()
     self.ui = Ui_MainWindow_Override()
+    self.ui.setupUi(self)
+
+class dbAddWindow(QMainWindow):
+  def __init__(self):
+    super(dbAddWindow, self).__init__()
+    self.ui = Ui_dbAddWindow_Override()
+    self.ui.setupUi(self)
+
+class dbDelWindow(QMainWindow):
+  def __init__(self):
+    super(dbDelWindow, self).__init__()
+    self.ui = Ui_dbDelWindow_Override()
     self.ui.setupUi(self)
 
 class dbFindWindow(QMainWindow):
@@ -70,6 +106,8 @@ class dbFindWindow(QMainWindow):
     self.ui.setupUi(self)
 
 app = QApplication(sys.argv)
-window = MainWindow()
+
+window = dbMainWindow()
 window.show()
+
 sys.exit(app.exec())
